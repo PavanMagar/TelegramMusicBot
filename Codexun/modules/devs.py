c2fb88d4cd10fc8783866b8c1a7be2ab078925a8
Creator Pavan <99827658+PavanMagar@users.noreply.github.com>
2022-05-25 17:55:43 +0530

    Update devs.py

--- Codexun/modules/devs.py
+++ Codexun/modules/devs.py
@@ -50,7 +50,7 @@ async def update(_, message: Message):
         await start_restart_stage(x.chat.id, x.message_id)
         os.execvp("python3", ["python3", "-m", "Codexun"])
     else:
-        await message.reply_text("It's Up To Date With The [Latest Version](https://github.com/PavanMagar/CodexunMusicBot)", disable_web_page_preview=True)
+        await message.reply_text("Bot is already in the **up-to-date** mode with [main version](https://github.com/PavanMagar/CodexunMusicBot)", disable_web_page_preview=True)
         
 async def aexec(code, client, message):
     exec(
