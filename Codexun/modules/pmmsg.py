4ed1bcaaf443efd00dec1a4ba0ae20b0ff6716ba
Creator Pavan <99827658+PavanMagar@users.noreply.github.com>
2022-05-25 14:06:52 +0530

    Update pmmsg.py

--- Codexun/modules/pmmsg.py
+++ Codexun/modules/pmmsg.py
@@ -8,5 +8,5 @@ from Codexun.config import (
 
 @USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
 async def pmPermit(client: USER, message: Message):
-  await USER.send_message(message.chat.id,"Hey, this is the assistant of music bot, if you want to make your own music bot then visit at @TeamCodexun and contact us!\n\nPowered by @Codexun")
+  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot, didn't have a time to talk with you 🙂 kindly join @TeamCodexun for getting support\n\nPowered by @Codexun")
   return
